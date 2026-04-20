06c2f0163fa89c6e1d244df8f9015159fd1b1212
Raj Vardhan <errvmaurya563@gmail.com>
2026-04-20 20:41:48 +0530

    Add initial HTML and JavaScript files for basic webpage setup
    - Created index.html with a simple structure and linked script.js
    - Added script.js with console logs and variable declarations to demonstrate JavaScript functionality

---
+++ JSBasic/script.js
@@ -0,0 +1,24 @@
+console.log("Hello World!");
+console.log("Hello World 12123");
+
+let a = 5;
+
+a = 10;
+
+let b = true;
+
+const pi = 3.14;
+
+
+console.log("task complete");
+
+let x = 10;
+let y = "10";
+
+console.log(typeof x);
+console.log(typeof y);
+
+
+console.log(x == y);
+
+console.log(x === y);
