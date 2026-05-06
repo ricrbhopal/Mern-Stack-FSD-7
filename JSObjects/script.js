265396499eaf017edcb9da88cace71b659b63957
Raj Vardhan <errvmaurya563@gmail.com>
2026-05-06 20:37:55 +0530

    Add initial HTML and JavaScript files for JSObjects functionality

---
+++ JSObjects/script.js
@@ -0,0 +1,95 @@
+// let fd = [
+//   ["RajVardhan", "18-11-1997", 7340862969, "Bhopal", "Teacher"],
+//   ["PranayDas", "18-11-1997", 7340862969, "WestBengal", "Teacher"],
+//   ["MohitPayasi", "18-11-1997", 7340862969, "Reva", "Teacher"],
+//   ["ArunavKishor", "18-11-1997", 7340862969, "Bina", "Teacher"],
+// ];
+
+// fd.forEach((element) => {
+//   console.log(element);
+// });
+
+// let FData = [
+//   {
+//     name: "AmitSharma",
+//     dob: "05-03-1995",
+//     phone: 9823456712,
+//     city: "Delhi",
+//     profession: "Engineer",
+//   },
+//   {
+//     name: "SnehaPatil",
+//     dob: "21-07-1998",
+//     phone: 9134567820,
+//     city: "Pune",
+//     profession: "Designer",
+//   },
+//   {
+//     name: "RohitVerma",
+//     dob: "11-12-1993",
+//     phone: 8765432190,
+//     city: "Lucknow",
+//     profession: "Developer",
+//   },
+//   {
+//     name: "NehaGupta",
+//     dob: "29-01-1997",
+//     phone: 9988776655,
+//     city: "Jaipur",
+//     profession: "Teacher",
+//   },
+// ];
+
+// FData.forEach((item) => {
+//   console.log(item["name"]);
+// });
+
+// FData.map((element, idx) => {
+//   console.log(idx);
+
+//   console.log(element.phone);
+// });
+
+// console.log(FData);
+// console.log(FData[1]["name"]);
+
+// let vishal = {
+//   name: "AmitSharma",
+//   dob: "05-03-1995",
+//   phone: 9823456712,
+//   city: "Delhi",
+//   profession: "Engineer",
+// };
+
+// console.log(Object.keys(vishal));
+// console.log(Object.values(vishal));
+// console.log(Object.entries(vishal));
+
+// let ar = [22, 43, 6, 74, 8, 91, 5, 100, 23, 6, 32, 6, 9, 4, 7, 92, 6, 45];
+
+// ar.sort((a, b) => a - b);
+
+// console.log(ar);
+
+// console.log(ar.find((val) => val === 6));
+// console.log(ar.findIndex((val) => val === 6));
+// console.log(ar.findLastIndex((val) => val === 6));
+
+// let ar2 = [ 4, 6, 74, 8, 91, 5, 100, 23, 6, 32, 6, 9, 4, 7, 92, 6, 45];
+
+// console.log(ar2.filter((val) => val > 20));
+
+// console.log(ar2.find((val) => val >20));
+
+// console.log(ar2.slice(0,7));
+
+
+let ab = "This is Web-Development Class"
+
+console.log(ab);
+
+console.log(ab.split(" "));
+console.log(ab.split("-"));
+
+
+
