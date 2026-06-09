82ac229c79d003ff74cb3536152dbcf451f73dd5
Raj Vardhan <errvmaurya563@gmail.com>
2026-06-09 20:54:50 +0530

    feat: add GridExample component and enhance App layout with new styles

--- Projects/Cravings/src/App.jsx
+++ Projects/Cravings/src/App.jsx
@@ -1,8 +1,14 @@
+import GridExample from "./components/GridExample";
+
 const App = () => {
   return (
     <>
+      <div className="p-3 h-50 bg-linear-to-b from-orange-500 to-white ">
+        something something
+      </div>
+      <GridExample />
       <div className="bg-[url('/commonBG.avif')] bg-cover h-screen w-full flex justify-center items-center">
-        <div className="w-2xs mx-auto border p-20 bg-white/70 rounded-2xl flex flex-col justify-center items-center">
+        <div className="w-5xl mx-auto border p-20 bg-white/70 rounded-2xl flex flex-col justify-center items-center">
           <p className="text-green-700">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
             culpa consequatur pariatur fuga tempora quam facilis, laboriosam
@@ -10,9 +16,25 @@ const App = () => {
             magnam nam blanditiis commodi ab! Rerum!
           </p>
 
-          <button className="bg-blue-500 px-5 py-2 mt-3 mx-5 rounded text-white hover:bg-blue-700 active:bg-amber-500">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="add any text"
+            className="border p-3 placeholder:text-red-600 bg-(--primary-color)"
+          />
+          <button className="bg-blue-500 px-5 py-2 mt-3 mx-5 rounded text-white hover:bg-blue-700 hover:text-red-600 active:bg-amber-500">
             submit
           </button>
+
+          <button className="mybtnDesign">xyz</button>
+
+          <div className="flex items-center gap-3 w-full">
+            <div className="border  border-fuchsia-800 w-full" />
+            <div>some text</div>
+            {/* <div className="border border-fuchsia-800 w-full " /> */}
+            <hr className="border border-fuchsia-800 w-full " />
+          </div>
         </div>
       </div>
     </>
---
+++ Projects/Cravings/src/components/GridExample.jsx
@@ -0,0 +1,172 @@
+const GridExample = () => {
+  return (
+    <>
+      <div className="grid grid-cols-[100px_25%_auto] gap-3">
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+        <div className="border" >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nostrum
+          hic voluptatum at quisquam sequi error officiis quibusdam, vero
+          laborum rerum maxime dolore veniam dicta soluta voluptas? Cum,
+          praesentium distinctio!
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GridExample;
