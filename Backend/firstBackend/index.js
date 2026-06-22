229d39fd1a431ece8514deb295d7187e3681e71d
Raj Vardhan <errvmaurya563@gmail.com>
2026-06-22 20:29:57 +0530

    feat: implement authentication and public routes with controllers

--- Backend/firstBackend/index.js
+++ Backend/firstBackend/index.js
@@ -2,16 +2,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import AuthRouter from "./src/routers/auth.route.js";
+import PublicRouter from "./src/routers/public.route.js";
 
 const app = express();
 
+app.use("/auth", AuthRouter);
+app.use("/public", PublicRouter);
+
+//Default API
 app.get("/", (req, res) => {
   console.log("Default Get API Hit");
   res.json({ message: "Welcome to my first backend Project" });
-});
-
-app.post("/login", (req, res) => {
-  res.json({ message: "Login Sucessfull" });
 });
 
 const port = process.env.PORT || 5000;
